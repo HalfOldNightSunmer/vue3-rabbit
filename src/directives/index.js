0665e542835c33b80d2a94b5510dfe38513f7575
半旧夜夏 <3523235371@qq.com>
2025-06-01 16:59:14 +0800

    perf(img):图片懒加载

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+import { useIntersectionObserver } from '@vueuse/core'
+// import {driective} from 'vue'
+
+export const lazyPlugin = {
+    install(app) {
+        app.directive('img-lazy', {
+            // bind 绑定  el:当前元素  binding:当前指令对象（=号后面的值）
+            mounted(el, binding) {
+                const {stop} = useIntersectionObserver(
+                    el,
+                    ([{  isIntersecting }]) => {
+                        if (isIntersecting) {
+                            console.log('进入可视区');
+                            el.src = binding.value
+                            stop()
+                        }
+                    }
+                )
+            }
+        })
+    }
+}
