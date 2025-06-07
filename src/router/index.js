b7a56ee1bb0db1f2c729b7dbc6bc9e5672841737
半旧夜夏 <3523235371@qq.com>
2025-06-07 23:34:54 +0800

    feat(category): 添加二级分类

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home/index.vue'
 import Login from '../views/Login/index.vue'
 import Layout from '../views/Layout/index.vue'
 import Category from '../views/Category/index.vue'
+import SubCategory from '../views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          name: 'subCategory',
+          component: SubCategory
         }
       ]
     },
@@ -29,8 +35,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: Login,
-    },
+    }
   ],
+  // 设置路由切换滚动条回到首页
+  scrollBehavior() {
+    return{
+      top: 0
+    }
+  }
 })
 
 export default router
